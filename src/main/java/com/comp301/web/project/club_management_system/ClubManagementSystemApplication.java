55e98dcac62683b33f4a417093713edf5d6a5e6a
Said Cemal Türkdoğan <69085609+saidturkdogan@users.noreply.github.com>
2024-12-07 16:56:27 +0300

    Initial commit

---
+++ src/main/java/com/comp301/web/project/club_management_system/ClubManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.comp301.web.project.club_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comp301/web/project/club_management_system/ClubManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp301.web.project.club_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
