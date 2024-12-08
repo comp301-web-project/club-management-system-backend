43cc409da351052aacc41019e6577599e07d3ef2
Said Cemal Türkdoğan <69085609+saidturkdogan@users.noreply.github.com>
2024-12-09 01:16:41 +0300

    Initial commit

---
+++ club-management-service/src/test/java/org/clubmanagementsystem/clubmanagementservice/ClubManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.clubmanagementsystem.clubmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-management-service/src/test/java/org/clubmanagementsystem/usermanagementservice/UserManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.clubmanagementsystem.usermanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
