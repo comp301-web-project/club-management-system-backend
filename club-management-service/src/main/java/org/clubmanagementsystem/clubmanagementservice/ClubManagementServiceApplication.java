1580f85b410988733a828f05afe7bedaaefeccc6
Said Cemal Türkdoğan <69085609+saidturkdogan@users.noreply.github.com>
2025-01-05 03:29:10 +0300

    Configure unique ports for microservices and update date handling

--- club-management-service/src/main/java/org/clubmanagementsystem/clubmanagementservice/ClubManagementServiceApplication.java
+++ club-management-service/src/main/java/org/clubmanagementsystem/clubmanagementservice/ClubManagementServiceApplication.java
@@ -1,9 +1,11 @@
 package org.clubmanagementsystem.clubmanagementservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ClubManagementServiceApplication {
 
 	public static void main(String[] args) {
--- event-management-service/src/main/java/org/clubmanagementsystem/eventmanagementservice/EventManagementServiceApplication.java
+++ event-management-service/src/main/java/org/clubmanagementsystem/eventmanagementservice/EventManagementServiceApplication.java
@@ -2,8 +2,9 @@ package org.clubmanagementsystem.eventmanagementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class EventManagementServiceApplication {
 
 	public static void main(String[] args) {
